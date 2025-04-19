2c85e057e0e39e172222293d442dc119f58c4ce3
vg15o2 <91283933+vg15o2@users.noreply.github.com>
2025-04-19 09:48:22 +0530

    Time: 23 ms (98.63%), Space: 60.5 MB (38.74%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    ranges::sort(nums);
+    return countLess(nums, upper) - countLess(nums, lower - 1);
+  }
+
+ private:
+  long countLess(const vector<int>& nums, int sum) {
+    long res = 0;
+    for (int i = 0, j = nums.size() - 1; i < j; ++i) {
+      while (i < j && nums[i] + nums[j] > sum)
+        --j;
+      res += j - i;
+    }
+    return res;
+  }
+};
