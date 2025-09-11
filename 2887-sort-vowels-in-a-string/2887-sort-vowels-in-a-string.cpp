ccd1be28d6e902a73fe4af78b352aee676c3562f
vaishnavi <91283933+vg15o2@users.noreply.github.com>
2025-09-11 10:31:14 +0530

    Time: 17 ms (43.62%), Space: 16 MB (70.34%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    string sortVowels(string s) {        
-        vector<int> count(128, 0);  
+    string sortVowels(string s) {
+        vector<int> count(128, 0);  // O(1) space
         
+        // Count vowels
         for (char c : s) {
-            if (isVowel(c)) {
-                count[c]++;
-            }
+            if (isVowel(c)) count[c]++;
         }
         
+        // Reconstruct with sorted vowels
         string vowels = "AEIOUaeiou";
         int vowelIdx = 0;
         
         for (char& c : s) {
             if (isVowel(c)) {
-                
-                while (vowelIdx < vowels.length() && count[vowels[vowelIdx]] == 0) {
-                    vowelIdx++;
-                }
+                while (count[vowels[vowelIdx]] == 0) vowelIdx++;
                 c = vowels[vowelIdx];
                 count[vowels[vowelIdx]]--;
             }
         }
-        
         return s;
     }
-    
 private:
-    inline bool isVowel(char c) {
+    bool isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
-};
+};    
